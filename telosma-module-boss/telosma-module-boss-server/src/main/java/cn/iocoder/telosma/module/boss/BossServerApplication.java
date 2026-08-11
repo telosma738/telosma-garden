8606e3c09858bee4b3b0fbafd4a2282cf3bab9d0
Telosma738 <jera738@163.com>
2026-08-12 00:19:46 +0800

    initial boss service

---
+++ telosma-module-boss/telosma-module-boss-server/src/main/java/cn/iocoder/telosma/module/boss/BossServerApplication.java
@@ -0,0 +1,9 @@
+package cn.iocoder.telosma.module.boss;
+
+import org.springframework.boot.SpringApplication;
+
+public class BossServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BossServerApplication.class, args);
+    }
+}
